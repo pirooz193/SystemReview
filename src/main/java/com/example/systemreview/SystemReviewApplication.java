8b6487a044fd8cb67a860669a6c572dd13878a0b
pirooz193 <piroozazarabad@gmail.com>
2023-06-23 10:39:51 +0330

    initial commit

---
+++ src/main/java/com/example/systemreview/SystemReviewApplication.java
@@ -0,0 +1,13 @@
+package com.example.systemreview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SystemReviewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SystemReviewApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/systemreview/SystemReviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.systemreview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SystemReviewApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
