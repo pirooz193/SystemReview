b0161733d54a1ab828363f3314212711f397f378
pirooz193 <piroozazarabad@gmail.com>
2023-06-24 15:08:07 +0330

    Create a method to get all products.

--- src/main/java/com/example/systemreview/service/ProductService.java
+++ src/main/java/com/example/systemreview/service/ProductService.java
@@ -2,10 +2,14 @@ package com.example.systemreview.service;
 
 import com.example.systemreview.service.dto.ProductDTO;
 
+import java.util.List;
+
 public interface ProductService {
     ProductDTO updateProductDetails(ProductDTO productDTO);
 
     ProductDTO getRequiredProduct(Long productId);
 
     ProductDTO createProduct(ProductDTO productDTO);
+
+    List<ProductDTO> getAllProducts();
 }
