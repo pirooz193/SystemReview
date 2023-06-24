41a3320a99daa81a83630a4bca1c21daff32862a
pirooz193 <piroozazarabad@gmail.com>
2023-06-24 11:55:26 +0330

    Create a mapper interface to map DTO and entity for Product. - Create a method to handle updating of a product and ignore null values.

---
+++ src/main/java/com/example/systemreview/service/mapper/ProductMapper.java
@@ -0,0 +1,22 @@
+package com.example.systemreview.service.mapper;
+
+import com.example.systemreview.domain.Product;
+import com.example.systemreview.service.dto.ProductDTO;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
+public interface ProductMapper {
+
+    Product toEntity(ProductDTO productDTO);
+
+    ProductDTO toDTO(Product product);
+
+    default Product updateProductDetailsFromDto(ProductDTO productDTO, Product product) {
+        if (productDTO.getName() != null) product.setName(productDTO.getName());
+        if (productDTO.getReactionRules() != null) product.setReactionRules(productDTO.getReactionRules());
+        if (productDTO.isPresentable() != null) product.setPresentable(productDTO.isPresentable());
+        if (productDTO.isCommentingEnabled() != null) product.setCommentingEnabled(productDTO.isCommentingEnabled());
+        if (productDTO.isVotingEnabled() != null) product.setVotingEnabled(productDTO.isVotingEnabled());
+        return product;
+    }
+}
