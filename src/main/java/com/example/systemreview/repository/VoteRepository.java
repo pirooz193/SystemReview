39318e0f37f18158553a348672f41f07ea140ff9
pirooz193 <piroozazarabad@gmail.com>
2023-06-24 14:59:33 +0330

    Created a method to calculate the average of a required product . - The required product should be approved

--- src/main/java/com/example/systemreview/repository/VoteRepository.java
+++ src/main/java/com/example/systemreview/repository/VoteRepository.java
@@ -2,8 +2,14 @@ package com.example.systemreview.repository;
 
 import com.example.systemreview.domain.Vote;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface VoteRepository extends JpaRepository<Vote, Long> {
+
+    //    Float calculateAverageScoreByProductId(Long productId);
+    @Query("SELECT AVG(v.score) FROM Vote v WHERE v.product.id = :productId and v.approvalStatus = :approvalStatus")
+    Float findAverageScoreByProductId(@Param("productId") Long productId, @Param("approvalStatus") Boolean approvalStatus);
 }
